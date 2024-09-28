abdf446e7cd1c898ba5a9edc0ac8ae5b58f84da0
Fidelis Odigwe <fidelisodigweuk@gmail.com>
2024-09-28 17:03:00 +0100

    Updated Random Quote Generator

--- Randon_Quote_Generator.cpp
+++ Randon_Quote_Generator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
-
+#include <cstdlib>
+#include <ctime>
+#include "Randon_Quote_Generator.h"
 using namespace std;
 
 // this is a Random Quote generator app that prints random quote based on users emotions
@@ -27,69 +29,122 @@ void selection_list()
 
    switch(choice){
 
-    case (1):{
-
-       char happy[] = {"Joy is not in things; it is in us.\n"};
-        cout << happy << endl;
+     case (1):{
+         string sadEmotion[] = {"Tears come from the heart and not from the brain.",
+                             "The word 'happy' would lose its meaning if it were not balanced by sadness.",
+                             "Every human walks around with a certain kind of sadness. They may not wear it on their sleeves,but it is there if you look deep",
+                             "The walls we build around us to keep sadness out also keeps out the joy",
+                             "Sadness flies away on the wings of time"};
+      srand(time(0));
+        int index = rand()%(sizeof(sadEmotion)/sizeof(sadEmotion[0]));
+        cout <<"Sad Emotion Quote: "<< sadEmotion[index] << endl;                     
+        cout << sadEmotion << endl;
         break;
+     
     }
     
-
-
-    case (2):{
-
-        char sad[] = {"Tears come from the heart and not from the brain.\n"};
-        cout << sad << endl;
+   case (2):{
+        string happyEmotion[] = {"Joy is not in things; it is in us.","Happiness is not something ready-made. It comes from your own actions.",
+                                "The only way to find true happiness is to risk being completely cut open.","Happiness depends upon ourselves",
+                                "The secret of happiness is not in doing what one likes, but in liking what one does"};
+        srand(time(0));
+        int index = rand()%(sizeof(happyEmotion)/sizeof(happyEmotion[0]));
+        cout <<"Happy Emotion Quote: "<< happyEmotion[index] << endl;
         break;
+       
     }
     
-    
-    case (3):{
-
-        char hot[] = {"Heat cannot be separated from fire, or beauty from the eternal.\n"};
-        cout << hot << endl;
+   case (3):{
+     
+       string coldEmotion[] = {"Winter is not a season, it's a celebration.",
+                                "Nothing burns like the cold.",
+                                "Winter is not a season, it's an occupation",
+                                "Revenge is a dish best served cold",
+                                "People who live in a cold climate are not cold in their hearts"};
+         srand(time(0));
+        int index = rand()%(sizeof(coldEmotion)/sizeof(coldEmotion[0]));
+        cout <<"Cold Emotion Quote: "<< coldEmotion[index] << endl;              
+        cout << coldEmotion << endl;
         break;
+       
     }
     
-    
-    case (4):{
-
-        char cold[] = {"Winter is not a season, it's a celebration.\n"};
-        cout << cold << endl;
+  case (4):{
+     string hotEmotion[] = {"Heat cannot be separated from fire, or beauty from the eternal.",
+                               "The most powerful weapon on earth is the human soul on fire",
+                             "Anger is like fire. It burns all clean",
+                             "Love is a burning thing, and it makes a fiery ring.",
+                             "Passion is energy. Feel the power that comes from focusing on what excites you."};
+                             srand(time(0));
+        int index = rand()%(sizeof(hotEmotion)/sizeof(hotEmotion[0]));
+        cout <<"Hot Emotion Quote: "<< hotEmotion[index] << endl; 
+        cout << hotEmotion << endl;
         break;
+     
     }
-    
     
     case (5):{
-
-       char tired[] = {"Even the darkest night will end, and the sun will rise.\n"};
-       cout << tired << endl;
-       break;
-    }
-    
-
-    
-    case (6):{
-
-        char energetic[] = {"Energy and persistence conquer all things.\n"};
-        cout << energetic << endl;
+     
+        string energeticEmotion[] = {"Energy and persistence conquer all things.",
+                           "Passion is energy. Feel the power that comes from focusing on what excites you",
+                           "Enthusiasm is the energy and force that builds literal momentum of the human soul and mind",
+                           "The more you lose yourself in something bigger than yourself, the more energy you will have",
+                           "Vitality shows in not only the ability to persist but the ability to start over"};
+                            srand(time(0));
+        int index = rand()%(sizeof(energeticEmotion)/sizeof(energeticEmotion[0]));
+        cout <<"Energetic  Emotion Quote: "<< energeticEmotion[index] << endl; 
+        cout << energeticEmotion << endl;
         break;
+      
+    }
+     
+      
+
+     case (6):{
+     string tiredEmotion[] = {"Even the darkest night will end, and the sun will rise.",
+                             "You never get tired unless you stop and take time for it.",
+                             "Even when you have every right to be tired, you must never be too tired to move forward.",
+                             "I’m tired of feeling like I’m fighting alone",
+                             "Tired minds don't plan well. Sleep first, plan later." };
+           srand(time(0));
+        int index = rand()%(sizeof(tiredEmotion)/sizeof(tiredEmotion[0]));
+        cout <<"Tired Emotion Quote: "<< tiredEmotion[index] << endl;                     
+       cout << tiredEmotion << endl;
+       break;
     }
     
     
     case (7):{
-
-        char confidence[] = {"Believe you can and you're halfway there.\n"};
-        cout << confidence << endl;
+      string timidEmotion[] = {"Fear is only as deep as the mind allows.",
+                              "Fortune favors the bold, but abandons the timid",
+                              "Timid men prefer the calm of despotism to the tempestuous sea of liberty",
+                              "A timid person is frightened before a danger, a coward during the time, and a courageous person afterward",
+                               "Do not be too timid and squeamish about your actions. All life is an experiment"};
+    
+         srand(time(0));
+        int index = rand()%(sizeof(timidEmotion)/sizeof(timidEmotion[0]));
+        cout <<"Timid  Emotion Quote: "<< timidEmotion[index] << endl; 
+        cout << timidEmotion << endl;
         break;
+     
     }
     
+      
+
+
     
     case (8):{
-
-        char timid[] = {"Fear is only as deep as the mind allows."};
-        cout << timid << endl;
+      string confidenceEmotion[] = {"Believe you can and you're halfway there.",
+                              "Confidence comes not from always being right, but from not fearing to be wrong",
+                              "Self-confidence is the best outfit. Rock it and own it",
+                              "With confidence, you have won before you have started.",
+                              "No one can make you feel inferior without your consent"};
+                          srand(time(0));
+        int index = rand()%(sizeof(confidenceEmotion)/sizeof(confidenceEmotion[0]));
+        cout <<"Confidence  Emotion Quote: "<< confidenceEmotion[index] << endl; 
+        cout << confidenceEmotion << endl;
         break;
+       
     }
     
     
@@ -115,9 +170,3 @@ void selection_list()
 
 
 
-int main()
-{
-
-  selection_list();
-    
-}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "Randon_Quote_Generator.h"
+using namespace std;
+
+
+int main()
+{
+
+  selection_list();
+    
+}
